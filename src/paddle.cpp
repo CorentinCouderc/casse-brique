3a1018afc7a5f4fdbe0cc8584be5d3789fb4e183
Maxime Castello <maxime.castello@cpe.fr>
2017-03-07 22:52:41 +0100

    Getters & Setters of Paddle

--- src/paddle.cpp
+++ src/paddle.cpp
@@ -10,3 +10,34 @@ paddle::paddle(vec2 position_pad,int width_pad,int height_pad)
      width(width_pad),
      height(height_pad)
 {}
+
+/** Getter for width member variable
+ * @return returns value of height
+ */
+int paddle::get_width()
+{
+    return width;
+}
+
+/** Getter for height member variable
+ * @return returns value of height
+ */
+int paddle::get_height()
+{
+    return height;
+}
+
+/** Setter for width member variable
+ */
+void paddle::set_width(int new_width)
+{
+    width = new_width;
+}
+
+/** Setter for height member variable
+ * @param new_height new height value to set height to
+ */
+void paddle::set_height(int new_height)
+{
+    height = new_height;
+}
--- src/paddle.hpp
+++ src/paddle.hpp
@@ -24,7 +24,28 @@ struct paddle
     /** Constructor paddle({x,y},w,h) */
     paddle(vec2 position, int width, int height);
 
+    /** Getter for width member variable
+     * @return returns value of height
+     */
+    int get_width();
+
+    /** Getter for height member variable
+     * @return returns value of height
+     */
+    int get_height();
+
+    /** Setter for width member variable
+     */
+    void set_width(int new_width);
+
+    /** Setter for height member variable
+     * @param new_height new height value to set height to
+     */
+    void set_height(int new_height);
+
 };
+
+
 
 #endif // PADDLE_HPP
 
